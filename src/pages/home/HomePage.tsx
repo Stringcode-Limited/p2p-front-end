eb48d5cf131446636a82e63194f3ded792ecbf8c
Daniel Egele <danielegele9@gmail.com>
2024-03-18 06:27:31 -0700

    all added files

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,28 @@
 import "./HomePage.css"
-function HomePage() {
+
+interface IHeader{
+  app: string,
+  home: string,
+  contactus:string,
+  faq:string,
+  login:string,
+
+}
+function HomePage({app, home, contactus, faq, login}:IHeader) {
   return (
     <>
     <div>
-      <header>
-        <menu>
+      <header className="Navbar">
+      <div className="P2P">{app}</div>
           <nav>
             <ul>
-              <li>Landing page</li>
+              <li><a href="">{home}</a></li>
+              <li><a href="">{contactus}</a></li>
+              <li><a href="">{faq}</a></li>
+              <li><a href="">{login}</a></li>
+
             </ul>
           </nav>
-        </menu>
       </header>
     </div>
     
