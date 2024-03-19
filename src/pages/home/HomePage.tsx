b26894da023cec1e2151533388c3952afd1877cc
Daniel Egele <danielegele9@gmail.com>
2024-03-19 08:40:33 -0700

    all added files

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,14 @@
 import Button from "../../components/Button/Button/"
 import "./HomePage.css"
 import iphone from "../../assets/images/iphone.png"
+import cbn from "../../assets/images/cbn.png"
+import ci from "../../assets/images/ci.png"
+import cis from "../../assets/images/cis.png"
+import fintech from "../../assets/images/fintech.png"
+import firs from "../../assets/images/firs.png"
+import fr from "../../assets/images/fr.png"
+import fraud from "../../assets/images/fraud.png"
+import sec from "../../assets/images/sec.png"
 
 
 
@@ -42,10 +50,39 @@ function HomePage() {
       </div>
       <div className="approved">
         <div className="sub-approved">
+          <div className="rap1">
           <h1 className="due">We are duly Approved</h1>
-          <p>Regulated by the Central Bank of Nigeria, licensed by Securities and Exchange Commission of Nigeria.</p>
+          <p className="aff">Regulated by the Central Bank of Nigeria, licensed by Securities and Exchange Commission of Nigeria.</p>
           <p className="aff">And affiliated to all necessary bodies in Nigeria and abroad.</p>
+           </div>
+          <div className="logos">
+            <div className="bank">
+          <img className="cbn" src={cbn} alt="" />
+          </div>
+          <div className="se">
+          <img className="sec" src={sec} alt="" />
+          </div>
+          <div className="fin">
+          <img className="fintech" src={fintech} alt="" />
+          </div>
+          <div className="frod">
+          <img className="fraud" src={fraud} alt="" />
+          </div>
+          <div className="ciso">
+          <img className="cis" src={cis} alt="" />
+          </div>
+          <div className="fro">
+          <img className="fr" src={fr} alt="" />
+          </div>
+          <div className="fs">
+          <img className="firs" src={firs} alt="" />
+          </div>
+          <div className="cii">
+          <img className="ci" src={ci} alt="" />
+          </div>
         </div>
+        </div>
+       
       </div>
     </div>
     
