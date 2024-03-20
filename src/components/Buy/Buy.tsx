2b4cfd1ff0aa1ddaf28ef114877978ceb12d63eb
hacker-elisha <etukudohelisha@gmail.com>
2024-03-20 22:29:04 +0100

    Added Buy and Sell Button

---
+++ src/components/Buy/Buy.tsx
@@ -0,0 +1,14 @@
+import './Buy.css'
+
+function Buy() {
+  return (
+    <>
+    <div className="Buy">
+        <div className="buyButton">Buy</div>
+        <div className="sellButton">Sell</div>
+    </div>
+    </>
+  )
+}
+
+export default Buy
