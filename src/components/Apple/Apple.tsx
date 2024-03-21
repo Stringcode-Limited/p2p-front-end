50c1d3202e2c319d3b6c9cb9d77ae6a768ccc55e
Daniel Egele <danielegele9@gmail.com>
2024-03-21 02:55:14 -0700

    all added files

---
+++ src/components/Apple/Apple.tsx
@@ -0,0 +1,17 @@
+import "./Apple.css";
+import { BsApple } from "react-icons/bs";
+
+function Apple() {
+  return (
+    <>
+      <div className="apple">
+        <div className="icon">
+        <BsApple />
+        </div>
+        <div className="text">Continue with Apple</div>
+      </div>
+    </>
+  );
+}
+
+export default Apple;
---
+++ src/components/Sell/Sell.tsx
@@ -0,0 +1,14 @@
+import './Sell.css'
+
+function Sell() {
+  return (
+    <>
+    <div className="Sell">
+        <div className="buyButton">Buy</div>
+        <div className="sellButton">Sell</div>
+    </div>
+    </>
+  )
+}
+
+export default Sell
