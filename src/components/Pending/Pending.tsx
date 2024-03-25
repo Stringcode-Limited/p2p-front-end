0ad3d8d3613ca7558de1f354e66cdd1a05316a66
hacker-elisha <etukudohelisha@gmail.com>
2024-03-25 18:24:53 +0100

    Added Orders Page, created cancelled, completed, and pending component

---
+++ src/components/Pending/Pending.tsx
@@ -0,0 +1,15 @@
+import './Pending.css'
+
+function Pending() {
+  return (
+    <>
+    <div className="Pending">
+        <div className="pendingButton">Pending</div>
+        <div className="completedButton">Completed</div>
+        <div className="cancelledButton">Cancelled</div>
+    </div>
+    </>
+  )
+}
+
+export default Pending
