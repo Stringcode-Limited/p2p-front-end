a0ce10c8beb42b94152ddeb0c06b46666cebf308
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-03-25 14:32:26 +0100

    updated login page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import {
   HomePage,
 } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 function App() {
   return (
     <>
@@ -17,6 +19,8 @@ function App() {
         <Route path="/" element={<HomeRouteWrapper />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
         </Route>
 
         <Route path="/app" element={<AppRouteWrapper />}>
@@ -27,6 +31,7 @@ function App() {
           <Route path="profile" element={<AppProfilePage />} />
           <Route path="signout" element={<AppSignOutPage/>}/>
         </Route>
+        
       </Routes>
     </>
   );
