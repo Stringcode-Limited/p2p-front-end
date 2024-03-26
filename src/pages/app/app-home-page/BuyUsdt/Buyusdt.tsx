08bfade227e014def17e68ae6951eb3fcadef7a8
Daniel Egele <danielegele9@gmail.com>
2024-03-26 05:18:01 -0700

    added features to buyusdt

--- src/pages/app/app-home-page/BuyUsdt/Buyusdt.tsx
+++ src/pages/app/app-home-page/BuyUsdt/Buyusdt.tsx
@@ -1,5 +1,8 @@
 import "./Buyusdt.css"
 import left from "../../../../assets/images/left.png"
+import OrderCard from "../../../../components/OrderCard/OrderCard";
+import figure from "../../../../assets/images/figure.png"
+import fiat from "../../../../assets/images/fiat.png"
 
 
 
@@ -9,13 +12,44 @@ import left from "../../../../assets/images/left.png"
 function Buyusdt() {
   return (
     <div className='usdt-info'>
-           
+        <div className="jr-usdt">
             <div className="middle-section">
-            <img src={left} alt="" />
+             <div className="left">
+            <div className="dt-icon">
+             <img src={left} alt="" />
+             </div>
+             <div className="dt">
+             <h3>Buy USDT</h3>
+             </div>
+             </div>
             </div>
-            
+            <div className="hero-frame">
+                <div className="figure-frame">
+                    <div className="inner-container">
+                        <div className="fig-bold">
+                            <div className="wrapps">
+                            <div className="fig-amount">
+                                <img src={figure} alt="" />
+                            </div>
+                            <div className="ud-label">
+                                <img src={fiat} alt="" />
+                            </div>
+                            </div>
+                            <div className="search">
+                              <input type="text" placeholder="# Enter Amount" />
+                            </div>
+                            <div className="amount">
+                                <p>#100,000.00 - #367,200.00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           </div>
            
+
     </div>
+    
   );
 }
 
