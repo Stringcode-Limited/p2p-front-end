52f1405bb07e8a9e7298dd859f75a5a4a4f32904
hacker-elisha <etukudohelisha@gmail.com>
2024-03-26 13:59:13 +0100

    Added Fiat and Crypto component

---
+++ src/components/Fiat/Fiat.tsx
@@ -0,0 +1,14 @@
+import './Fiat.css'
+
+function Fiat() {
+  return (
+    <>
+    <div className="fiat">
+        <div className="fiatButton">By Fiat</div>
+        <div className="cryptoButton">By Crypto</div>
+    </div>
+    </>
+  )
+}
+
+export default Fiat
