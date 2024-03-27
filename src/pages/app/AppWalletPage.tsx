84e8b823044af6193a04afbd7d72b0d2916404ea
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-03-27 22:50:28 +0100

    added wallet page

--- src/pages/app/AppWalletPage.tsx
+++
@@ -1,8 +0,0 @@
-
-function AppWalletPage() {
-  return (
-    <div>AppWalletPage</div>
-  )
-}
-
-export default AppWalletPage
---
+++ src/pages/app/app-wallet-page/AppWalletBody/AppWalletBody.tsx
@@ -0,0 +1,21 @@
+import "./appWalletBody.css";
+import left from '../../../../assets/images/left.png';
+
+
+
+const AppWalletBody = () => {
+  return (
+    <>
+        <div className="walletBody">
+            <div className="top">
+                <img src={left}/>
+                <p className='wallet'>Wallet</p>
+            </div>
+            
+            
+        </div>
+    </>
+  )
+}
+
+export default AppWalletBody
