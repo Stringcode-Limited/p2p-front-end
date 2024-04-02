332d33c07a011de3622c5df95267fcef7eae597d
hacker-elisha <etukudohelisha@gmail.com>
2024-04-02 14:36:07 +0100

    Changes

--- src/pages/app/app-home-page/Makepayment/MakePayment.tsx
+++ src/pages/app/app-home-page/Makepayment/MakePayment.tsx
@@ -96,7 +96,7 @@ function MakePayment() {
                         </div>
                     </div>
                        <div className="proceed-topay">
-                        <Button  type='large' text='Mark as paid' route="/app/transactiondetails"/>
+                        <Button  type='large' text='Mark as paid' route="/app/releasingcrypto"/>
                         <Button  type='large' text="help" color='grey'/>
                        </div>
                        <div className="help"><img src={help2} alt="" /></div>
