4d417e1745facf2cc2e3ab3e0942e32feda6a0e6
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-04-03 13:39:48 +0100

    updated changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,14 +24,10 @@ import CryptoSold from "./pages/app/app-home-page/cryptoSold/CryptoSoldPage/Cryp
 import WaitingPaymentPage from "./pages/app/app-home-page/waitingPayment/waitingPaymentPage/WaitingPaymentPage";
 import SellUsdtPage from "./pages/app/app-home-page/sellUsdt/sellUsdtPage/SellUsdtPage";
 import ManageCryptoPage from "./pages/app/app-wallet-page/ManageCrypto/ManageCryptoPage/ManageCryptoPage";
-<<<<<<< HEAD
-import TransactionDetails from "./pages/app/app-wallet-page/transactiondetails/TransactionDetails";
-
-=======
 import TransferPage from "./pages/app/app-wallet-page/Transfer/TransferPage/TransferPage";
 import VerifyWithPinPage from "./pages/app/app-wallet-page/VerifyWithPin/VerifyWithPinPage/VerifyWithPinPage";
 import CompletePage from "./pages/app/app-wallet-page/complete/CompletePage/CompletePage";
-import transactionDetails from './pages/app/app-home-page/Transactiondetails/transactionDetails';
+
 
 function App() {
   return (
@@ -59,7 +55,6 @@ function App() {
           <Route path="order" element={<AppOrderPage />} />
           <Route path="pitch" element={<AppPitchPage />} />
           <Route path="wallet" element={<AppWalletPage />} />
-          <Route path="transactiondetails" element={<TransactionDetails />} />
           <Route path="wallet/manageCrypto" element={<ManageCryptoPage/>} />
           <Route path="wallet/transfer" element={<TransferPage/>} />
           <Route path="wallet/verifywithpin" element={<VerifyWithPinPage/>} />
