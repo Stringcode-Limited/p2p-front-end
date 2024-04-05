ec54f0f90d253a82e3deb486441d49a428941b0b
Daniel Egele <danielegele9@gmail.com>
2024-04-05 04:24:46 -0700

    buy usdt features added

--- src/pages/app/app-home-page/BuyUsdt/BuyusdtBody/BuyUsdtBody.tsx
+++ src/pages/app/app-home-page/BuyUsdt/BuyusdtBody/BuyUsdtBody.tsx
@@ -96,9 +96,17 @@ function BuyusdtBody() {
 
                     
                 </div>
-                <div className="dividing-line">
-                        
-                        </div>
+                <div className="dividing-line"></div>
+
+                <div className="review-container">
+                    <div className="review-statement">
+                        <p className="reviews">Reviews & ratings</p>
+                    </div>
+                    <div className="review-comments">
+                        <p className="comments">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+
             </div>
            </div>
   );
