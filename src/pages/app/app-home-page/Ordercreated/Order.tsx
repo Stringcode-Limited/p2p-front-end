fddd1130954aa71567a8b5729390a98cf2d96642
Daniel Egele <danielegele9@gmail.com>
2024-04-05 17:18:37 -0700

    order created features added

--- src/pages/app/app-home-page/Ordercreated/Order.tsx
+++ src/pages/app/app-home-page/Ordercreated/Order.tsx
@@ -15,26 +15,16 @@ import naira from "../../../../assets/images/naira.png"
 function Order() {
   return (
     <div className='ordercreated'>
-      
-         <div className="order1">
-          <div className="order-caption">
-            <div className="arrow">
-              <div className="rights">
-              <img src={left} alt="" />
-              </div>
-              <div className="ders">
-              <h2>Order Created</h2>
-              </div>
+             <div className="top">
+         <img src={left} alt="" />
+        <p className="order-created-text">Order Created</p>
             </div>
-           
-          </div>
             <div className="customer-info">
-            <div className="jhn-d">
                          <div className="complete">
+                        <div className="nam-wrapper">
                         <div className="doe-wrapper">
                         <img src={doe} alt="" />
                         </div>
-                        <div className="nam-wrapper">
                           <div className="wraps">
                             <div className="name-wrapper">
                             <h4 className="jh">JohnDoe</h4>
@@ -50,9 +40,7 @@ function Order() {
                               <img src={right} alt="" />
                             </div>
                            </div>
-                          </div>
-                        
-                        <div className="make-payment">
+                           <div className="make-payment">
                             <div className="interval">
                               <div className="get">
                               <img src={clock} alt="" />
@@ -71,6 +59,9 @@ function Order() {
                               
                             </div>
                         </div>
+                          </div>
+                        
+                       
                         <div className="fiat-amount">
                           <div className="inner-fiat">
                             <div className="fiat1">
@@ -103,61 +94,10 @@ function Order() {
                         <Button  type='large' text='Proceed to pay'/>
                         <Button  type='large' text='Cancel order' color='grey'/>
                        </div>
-                    </div>
+                    
             </div>
-         </div>
-         <div className="chats">
-          <div className="chat-pertition">
-           <div className="chat-main">
-            <div className="chat-caption">
-              <p className='chat-text'>Chat</p>
-            </div>
-           <div className="interval">
-             <img src={interval} alt="" />
-            </div>
-            <div className="fiat-proceed">
-              <div className="fiat-amt">
-                <div className="merg-fiat">
-                <p className='iat-amount'>Fiat amount</p>
-                <p className='nai-fig'><img className='naira' src={naira} alt="" />5,000.00</p>
-                </div>
-              </div>
-              <div className="fiat-btn">
-                <Button  type='small' text="Proceed to pay" route='/app/Makepayment'/>
-              </div>
-            </div>
-            <div className="chat1">
-              <p className='stipulated'>You have successfully created an order. Kindly pay within the stipulated time limit</p>
-            </div>
-            <div className="chat2">
-              <div className="inner-chat">
-                <p className='semi'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
-                <div className="inner-chat">
-                <p className='semi'>Relit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              </div>
-             
-            </div>
-            <div className="chat3">
-              <div className="p3">
-                 <p className='p3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
-              </div>
-            </div>
-            <div className="chat4">
-              <div className="p4">
-                <p className='p4'>Thank you</p>
-              </div>
-            </div>
-           </div>
-            </div>
-            
-           <div className="type-message">
-               <div className="input">
-                <input type="text" placeholder='Type message' />
-                </div>
-                <img className='curv' src={curv} alt="" />
-           </div>
-         </div>
+         
+         
          
     </div>
   );
