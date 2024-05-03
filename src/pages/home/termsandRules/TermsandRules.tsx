f951703730ebc297216a5e64ea8790271adeed8d
Daniel Egele <danielegele9@gmail.com>
2024-05-03 05:43:54 -0700

    terms and condition features added

--- src/pages/home/termsandRules/TermsandRules.tsx
+++ src/pages/home/termsandRules/TermsandRules.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import NavBar from '../../../components/NavBar/NavBar';
+import "./termsandRules.css"
 
 function TermsandRules() {
   return (
     <div className='terms-rules'>
         <NavBar />
+        <div className="terms-rules-container">
+            <div className="terms-rules-inner-container">
+                <div className="terms-rules-text">
+                    <h1>Terms & Rules</h1>
+                </div>
+                <div className="terms-rules-condition-statements">
+                   <div className="p1">
+                    <p className='terms'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id luctus mauris, eu eleifend urna. Nunc ac eros in ante facilisis facilisis. In lobortis lacinia velit a elementum. Pellentesque tempor nisl et lorem mattis commodo. Sed fringilla dui vel semper egestas. Maecenas eu ultrices ligula. Curabitur sit amet diam porta, pellentesque lacus et, interdum tellus. Nulla accumsan dolor sit amet lacinia euismod. Nullam malesuada luctus ultrices. Praesent pretium nisl tellus, non malesuada lorem accumsan sit amet. Nulla ut viverra magna. Nullam consectetur blandit lectus at suscipit.</p>
+                    </div>
+                    <div className="p1">
+                    <p className='terms'>Donec lobortis accumsan massa dictum finibus. Phasellus euismod metus eget diam ornare, quis efficitur nibh fermentum. Nullam gravida ac nisi vitae suscipit. Pellentesque orci mauris, consequat quis gravida sed, convallis ut risus. Nullam mattis urna elit, sed dapibus justo condimentum ut. Ut hendrerit semper massa vitae iaculis. Morbi aliquam arcu sed nibh facilisis fringilla condimentum eget ipsum.</p>
+                    </div>
+                    <div className="p1">
+                     <p className='terms'>Donec lobortis accumsan massa dictum finibus. Phasellus euismod metus eget diam ornare, quis efficitur nibh fermentum. Nullam gravida ac nisi vitae suscipit. Pellentesque orci mauris, consequat quis gravida sed, convallis ut risus. Nullam mattis urna elit, sed dapibus justo condimentum ut. Ut hendrerit semper massa vitae iaculis. Morbi aliquam arcu sed nibh facilisis fringilla condimentum eget ipsum.</p>
+                    </div>
+                    <div className="p1">
+                    <p className='terms'> Maecenas eu ultrices ligula. Curabitur sit amet diam porta, pellentesque lacus et, interdum tellus. Nulla accumsan dolor sit amet lacinia euismod. Nullam malesuada luctus ultrices. Praesent pretium nisl tellus, non malesuada lorem accumsan sit amet. Nulla ut viverra magna. Nullam consectetur blandit lectus at suscipit.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
       
     </div>
   );
